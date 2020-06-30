df7ca8076e1182d7ded3ca32af2b0e782a822c0a
hillarioh <okerioh@gmail.com>
2020-06-30 05:22:13 +0300

    add line model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_144111) do
+ActiveRecord::Schema.define(version: 2020_06_30_021957) do
+
+  create_table "lines", force: :cascade do |t|
+    t.string "text"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_lines_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_06_29_144111) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "lines", "users"
 end
