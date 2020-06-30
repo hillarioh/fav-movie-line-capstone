c529e35f586ce5588246e952ecf943a1606726c5
hillarioh <okerioh@gmail.com>
2020-06-30 08:59:17 +0300

    add test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Check for validation of user input' do
+    it 'is valid with valid attributes' do
+      expect(User.create).to_not be_valid
+    end
+
+    it 'is not valid without a username' do
+      expect(User.new(username: nil)).to_not be_valid
+    end
+
+    it 'is not valid without a fullname' do
+      expect(User.new(fullname: nil)).to_not be_valid
+    end
+  end
+
+  
 end
