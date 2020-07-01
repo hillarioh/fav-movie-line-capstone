0c6a908c3a24308321e973bc5d060e28f1d1aa18
hillarioh <okerioh@gmail.com>
2020-07-01 11:13:24 +0300

    add active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
     has_many :lines, foreign_key: 'author_id', class_name: 'Line'
+    has_many_attached :images
 
     validates_presence_of :username
     validates_presence_of :fullname
