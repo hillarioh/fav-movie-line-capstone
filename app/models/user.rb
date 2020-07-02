59d9c91f61a56a2cb0d80b70415fecba32ba9f15
hillarioh <okerioh@gmail.com>
2020-07-02 11:02:13 +0300

    add following model, add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
     has_one_attached :profile_pic
     has_one_attached :cover_pic
 
+    has_many :followings
+    has_many :followers, class_name: 'Following', foreign_key: 'follower_id'
+
     validates_presence_of :username
     validates_presence_of :fullname
 end
