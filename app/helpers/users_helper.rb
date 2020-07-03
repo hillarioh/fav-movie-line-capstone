900c4d650143b18f126e6a10f8d23af6c425f794
hillarioh <okerioh@gmail.com>
2020-07-03 04:55:54 +0300

    edit index action, view, add check status method, scope

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+
+    def check_status(user)
+        Following.has_followed(user,current_user).count.positive? ? false : true       
+    end
 end
--- app/models/following.rb
+++ app/models/following.rb
@@ -3,5 +3,7 @@ class Following < ApplicationRecord
     belongs_to :followed,class_name: 'User'
     belongs_to :follower,class_name: 'User'
 
+    scope :has_followed, ->(u,c) { where(followed: u,follower: c) }
+
     
 end
