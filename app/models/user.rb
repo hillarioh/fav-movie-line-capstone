b3897ee27b1231f052967b339eb4aebedc63127b
hillarioh <okerioh@gmail.com>
2020-07-04 07:59:33 +0300

    remove linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  after_commit :add_default_profile, on: [:create, :update]
-  after_commit :add_default_cover, on: [:create, :update]
-
+  after_commit :add_default_profile, on: %i[create update]
+  after_commit :add_default_cover, on: %i[create update]
 
   has_many :lines, foreign_key: 'author_id', class_name: 'Line'
   has_one_attached :profile_pic
@@ -12,19 +11,20 @@ class User < ApplicationRecord
 
   validates_presence_of :username
   validates_presence_of :fullname
-  
-  private 
+
+  private
+
   def add_default_profile
-    unless profile_pic.attached?
-      self.profile_pic.attach(io: File.open(Rails.root.join("app", "assets", "images", "default-prof.png")), filename: 'default-prof.png' , content_type: "image/png")
-    end
+    return if profile_pic.attached?
+
+    file = File.open(Rails.root.join('app', 'assets', 'images', 'default-prof.png'))
+    profile_pic.attach(io: file, filename: 'default-prof.png', content_type: 'image/png')
   end
 
   def add_default_cover
-    unless cover_pic.attached?
-      self.cover_pic.attach(io: File.open(Rails.root.join("app", "assets", "images", "default-cover2.jpg")), filename: 'default-cover2.jpg' , content_type: "image/jpg")
-    end
-  end
+    return cover_pic.attached?
 
- 
+    file = File.open(Rails.root.join('app', 'assets', 'images', 'default-cover2.jpg'))
+    cover_pic.attach(io: file, filename: 'default-cover2.jpg', content_type: 'image/jpg')
+  end
 end
