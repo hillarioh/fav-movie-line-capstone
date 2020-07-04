29ef003f0ab1c7f6ef9e97fd9547afe7a2c1da1f
hillarioh <okerioh@gmail.com>
2020-07-04 07:38:59 +0300

    add default image, callback, action and remove image tag condition

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  after_commit :add_default_profile, on: [:create, :update]
+  after_commit :add_default_cover, on: [:create, :update]
+
+
   has_many :lines, foreign_key: 'author_id', class_name: 'Line'
   has_one_attached :profile_pic
   has_one_attached :cover_pic
@@ -8,6 +12,19 @@ class User < ApplicationRecord
 
   validates_presence_of :username
   validates_presence_of :fullname
-  validates_presence_of :profile_pic
-  validates_presence_of :cover_pic
+  
+  private 
+  def add_default_profile
+    unless profile_pic.attached?
+      self.profile_pic.attach(io: File.open(Rails.root.join("app", "assets", "images", "default-prof.png")), filename: 'default-prof.png' , content_type: "image/png")
+    end
+  end
+
+  def add_default_cover
+    unless cover_pic.attached?
+      self.cover_pic.attach(io: File.open(Rails.root.join("app", "assets", "images", "default-cover2.jpg")), filename: 'default-cover2.jpg' , content_type: "image/jpg")
+    end
+  end
+
+ 
 end
